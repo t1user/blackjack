c2bbbb78063345f38de966ca215fe9018a3b6219
Tomasz <tomasz2605@gmail.com>
2016-08-19 15:15:34 +0200

    Most classes moved to kv.

--- bj_kv_2.py
+++ bj_kv_2.py
@@ -3,7 +3,7 @@
 from kivy.app import App
 from kivy.uix.image import Image
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ListProperty
+from kivy.properties import ListProperty, ObjectProperty
 from blackjack import *
 
 class CardView(BoxLayout):
@@ -16,23 +16,25 @@ class PlayArea(BoxLayout):
     def __init__ (self, **kwargs):
         super().__init__(**kwargs)
 
-        playercards = player_hand.cards
-                
+        self.playercards = player_hand.cards
+        
         dealer_screen = CardView()
         player_screen = CardView()
+        
         self.k_play(dealer_screen, dealer.hand)
         self.k_play(player_screen, player_hand)
         
         self.add_widget(dealer_screen)
         self.add_widget(player_screen)
         
-        print(playercards)
+        print(self.playercards)
         player_hand.delete()
-        print(playercards)
+        self.playercards = player_hand.cards
+        print(self.playercards)
+        self.playercards = player_hand.cards
         player_hand.deal()
-        print(playercards)
-        
-        self.on_playercards()
+        self.playercards = player_hand.cards
+        print(self.playercards)
         
     def k_play(self, screen, hand):
         screen.clear_widgets()
@@ -40,57 +42,17 @@ class PlayArea(BoxLayout):
             screen.add_widget(i.image)
     
     def on_playercards(self, *args):
-        self.k_play(player_screen, player_hand)
-        print(playercards)
+        #self.k_play(self.player_screen, self.player_hand)
+        print(self.playercards)
         print(args)
 
-class ButtonStrip(BoxLayout):
-       def hit(self):
-        player_hand.deal()
-    
-class InfoStrip(BoxLayout):
-    pass
-
-class Outer(BoxLayout):
-    pass
-
-"""
-class PointLabel(Label):
-    pass
-
-class ScoreLabel(Label):
-    pass
-"""
-
 class Screen(BoxLayout):
-    def __init__ (self, **kwargs):
-        super().__init__(**kwargs)
-
-
-        
-        
-        """             
-
-                infostrip = InfoStrip()
-                outer = Outer()
-                pa = PlayArea()
-                buttons = ButtonStrip()
-        """
-
-
-        
-"""
-        pa.add_widget(dealer_screen)
-        pa.add_widget(player_screen)
-        outer.add_widget(infostrip)
-        outer.add_widget(pa)
-        self.add_widget(outer)
-        self.add_widget(buttons)
-"""
-               
-
-class Bj2App(App):
     pass
+             
+class Bj2App(App):
+    def hit(self):
+        player_hand.deal()
+        print(player_hand.cards)
     
 if __name__ == '__main__':
     shoe = Shoe()
