fa5eabfe2c64306c21df3d45fe64cbd0c528cc06
Tomasz <tomasz2605@gmail.com>
2016-08-25 03:31:54 +0200

    Split not working

--- bj_kv_6.py
+++ bj_kv_6.py
@@ -5,7 +5,8 @@ from kivy.uix.image import Image
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.label import Label
-from kivy.properties import ListProperty, ObjectProperty, NumericProperty
+from kivy.properties import ListProperty, ObjectProperty, \
+                            NumericProperty, StringProperty
 
 from blackjack_1 import Controller
 
@@ -17,26 +18,49 @@ class CardView(RelativeLayout):
 class PointsLabel(Label):
     pass
 
+class InsuranceButtons(BoxLayout):
+    def yes(self):
+        c.buy_insurance()
+        s.game_controller()
+    
+    def no(self):
+        c.decline_insurance()
+        s.game_controller()
+
+class PostEvaluationButtons(BoxLayout):
+    def deal(self):
+        c.player.bet = s.bet_size.value
+        c.run()
+        s.game_controller()
+        
+    def start_over(self):
+        pass
+
 class PlayActionButtons(BoxLayout):
     
-    stay_button = ObjectProperty()
+    active_options = ListProperty([1, 1, 1, 1])
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+        self.active_options = c.options
+    
+    def stand(self):
+        c.stand(c.player_hand)
+        s.game_controller()
 
-    def hit(self):
-        c.hit(c.player_hand)
-        s.playerhands = deepcopy(c.player.hands)
-        self.stay_button.disabled = True
+    def split(self):
+        c.split(c.player_hand)
+        s.game_controller()
         
     def double(self):
         c.double(c.player_hand)
-        s.playerhands = deepcopy(c.player.hands)   
-    
-    def split(self):
-        c.split(c.player_hand)
-        s.playerhands = deepcopy(c.player.hands)
-    
-    def stay(self):
-        c.stand(c.player_hand)
+        s.game_controller()      
 
+    def hit(self):
+        c.hit(c.player_hand)
+        s.game_controller()
+        
     
 class Screen(BoxLayout):
     
@@ -47,6 +71,7 @@ class Screen(BoxLayout):
     buttonstrip = ObjectProperty()
     cash_label = ObjectProperty()
     bet_size = ObjectProperty()
+    result_message = StringProperty()
     
     pos_para = NumericProperty(.4)
     pos_para_dealer = NumericProperty(.4)
@@ -58,10 +83,29 @@ class Screen(BoxLayout):
         self.playerhands = c.player.hands
         self.dealercards = c.dealer.hand.cards
         
+        #self.result_message = hand.result_message
+        
         self.x_card_offset = 35
         self.y_card_offset = 6
         
-        self.buttonstrip.add_widget(PlayActionButtons())
+        self.game_controller()
+    
+    def game_controller(self):
+        self.buttonstrip.clear_widgets()
+        self.playerhands = deepcopy(c.player.hands)
+        self.dealercards = c.dealer.hand.cards
+        self.bet_size.disabled = True
+        if c.insurance_option:
+            self.buttonstrip.add_widget(InsuranceButtons())
+        else:
+            c.player_input(c.player_hand)
+            if c.player_hand.played:
+                self.dealercards = c.dealer.hand.cards
+                self.cash_label.text = '{:,.0f}'.format(c.player.cash)
+                self.bet_size.disabled = False
+                self.buttonstrip.add_widget(PostEvaluationButtons())
+            else:
+                self.buttonstrip.add_widget(PlayActionButtons())
         
     def k_play(self, screen, hand):
         screen.clear_widgets()
@@ -81,7 +125,7 @@ class Screen(BoxLayout):
     
     def on_playerhands(self, *args):
         self.player_screen.clear_widgets()
-        self.hand_spacing = max(150 - max(len(self.playerhands)-3, 0)
+        self.hand_spacing = max(150 - max(len(self.playerhands)-2, 0)
          * 75, 20)
         self.pos_para = max(.4 - len(self.playerhands)/10, 0)
         if len(self.playerhands) > 2:
@@ -98,11 +142,11 @@ class Screen(BoxLayout):
     def update_player_hand(self, screen, hand):
         self.k_play(screen, hand)
         screen.add_widget(Label(
-        text='',
-        font_size='25dp',
-        pos_hint={'center_x': .5, 'center_y': .1}
+        text = hand.result_message,
+        font_size = '25dp',
+        pos_hint = {'center_x': .5, 'center_y': .1}
         ))
-        self.cash_label.text = '{:,}'.format(c.player.cash)
+        self.cash_label.text = '{:,.0f}'.format(c.player.cash)
         return screen
         
     def on_dealercards(self, *args):
--- blackjack_1.py
+++ blackjack_1.py
@@ -46,6 +46,7 @@ class Hand:
         self.doubled = False
         self.split = False
         self.bet = bet
+        self.result_message = ''
         if len(self.cards) == 0:
             self.deal()
        
@@ -53,7 +54,7 @@ class Hand:
         value = 0
         soft_value = 0
         if self.is_blackjack() and not self.split:
-            return 'BlackJack'
+            return 'BJ'
         for card in self.cards:
             if card.rank in FACES:
                 card_value = 10
@@ -86,25 +87,9 @@ class Hand:
 
     def deal(self):
         self.cards.append(self.shoe.contents.pop())
-    """        
-        def delete(self):
-            e = self.cards.pop()
 
-        def set_played(self):
-            self.played = True
-            return
-
-        def set_doubled(self):
-            self.doubled = True
-            return
-
-        def set_split(self):
-            self.split = True
-            return
-    """
     def __repr__(self):
         return str(self.cards)
-    
 
 class Dealer:
     def __init__(self, **kwargs):
@@ -114,13 +99,13 @@ class Dealer:
         else:
             self.hand = Hand()
         
-    def play(self, player_hand):
+    def play(self, player_hand, **kwargs):
         if player_hand.is_blackjack():
             for card in itertools.chain(FACES, 'A'):
                 if card in self.hand.get_ranks():
                     self.hand.deal()
                     if self.hand.is_blackjack:
-                        return 'BlackJack'
+                        return 'BJ'
                     return
             return
         if player_hand.is_bust:
@@ -130,7 +115,7 @@ class Dealer:
         while not self.hand.is_bust:
             if self.hand.get_value() < 17:
                 self.hand.deal()
-                if self.hand.get_value() == 'BlackJack':
+                if self.hand.get_value() == 'BJ':
                     return
             else:
                 break
@@ -152,71 +137,109 @@ class Controller:
         global shoe
         shoe = self.shoe
         self.player = Player()
+        self.insurance_option = False
+        self.insurance_asked = False
     
     def run(self):
         if len(self.shoe.contents) <= self.shoe.cut_card:
             print('\n!!!SHUFFLE!!!\n')
-            shoe = Shoe()
+            self.shoe = Shoe()
 
 #        if player.cash < player.bet:
 #            print()
 #            print("YOU LOST ALL YOUR MONEY. YOU MAKE ME SICK YOU STUPID FUCK!!!")
 #            break
+        self.player.hands = []
+        print('hands cleared')
         self.player_hand = Hand(bet=self.player.bet)
         self.dealer = Dealer()
         self.player_hand.deal()
         self.player.hands.append(self.player_hand)
         self.play(self.player_hand)
-        #self.dealer.play(self.player_hand)
+        self.insurance_asked = False
 
     def play(self, hand):
+        print('play', hand)
         self.bet = min(self.player.bet, self.player.cash)
         if hand.played:
             return
         self.player.cash -= self.bet
         if 'A' in self.dealer.hand.get_ranks():
-            if not self.player.insurance:
-                while True:
-                    response = input('Insurance? (Y/N)').upper()
-                    if response == 'Y':
-                        self.player.insurance = True
-                        self.player.cash -= 0.5 * self.bet
-                        print('Insurance purchased'.center(80))
-                        break
-                    elif response == 'N':
-                        break    
+            if not self.player.insurance and not self.insurance_asked:
+                self.insurance_option = True   
         if hand.is_blackjack():
             hand.played = True
-            return 'BlackJack'
+            return 'BJ'
+        """
         else:
-            if hand.doubled:
+            if hand.doubled :
                 return
             self.player_input(hand)
-
+        """
+        
     def player_input(self, hand):
-        if not hand.is_bust:
-            if hand.get_value() == 21:
-                hand.played = True
-                return
-            self.options = ['(H)it', '(S)tand']
+        if hand.get_value() == 21:
+            hand.played = True
+        elif hand.is_bust:
+            hand.played = True
+            return
+        elif not hand.played:
+            self.options = [1, 0, 0, 1]
             if len(hand.cards) == 2:
-                self.options.append('(D)ouble')
+                self.options = [1, 0, 1, 1]
                 if hand.get_ranks()[0] == hand.get_ranks()[1]:
-                    self.options.append('s(P)lit')
+                    self.options = [1, 1, 1, 1]
                 elif hand.get_ranks()[0] in FACES \
                      and hand.get_ranks()[1] in FACES:
-                    self.options.append('s(P)lit')
-            
-            #self.play = []
+                    self.options = [1, 1, 1, 1]
 
+        for hand in self.player.hands:
+            if not hand.played:
+                self.player_hand = hand
+                if len(self.player_hand.cards) < 2:
+                    self.player_hand.deal()
+                return
+            else:
+                print('running dealer')
+                self.dealer.play(self.player_hand, player=self.player)
+                self.final()
+    
+    def buy_insurance(self):
+        self.player.insurance = True
+        self.player.cash -= 0.5 * self.bet
+        self.insurance_option = False
+        self.insurance_asked = True
+
+    def decline_insurance(self):
+        self.player.insurance = False
+        self.insurance_asked = True
+        self.insurance_option = False
+    
     def play_all_hands(self):
-        for hand in self.hands:
-            if hand.played:
+        for hand in self.player.hands:
+            self.play(hand)
+            
+            """
+            self.player_hand = hand
+            if self.player_hand.played:
                 continue
             else:
-                hand.deal()
-                self.play(hand)
-            
+                print('playing hand: ', self.player_hand)
+                self.player_hand.deal()
+                self.play(self.player_hand)
+            """
+
+    def split(self, hand):
+        index = self.player.hands.index(hand)
+        self.player.hands.remove(hand)
+        i = 0
+        for card in hand.cards:
+            split_hand = Hand(card, self.player.bet)
+            split_hand.split = True
+            self.player.hands.insert(index + i, split_hand)
+            i += 1
+        self.play_all_hands()  
+             
     def hit(self, hand):
         hand.deal()
         
@@ -229,18 +252,48 @@ class Controller:
         hand.doubled = True
         hand.played = True
         hand.bet *= 2
+ 
+    def final(self):
+        print('entering final')
+        for hand in self.player.hands:
+            hand.result_message = self.evaluate(hand, self.dealer.hand)
+            self.player.cash += self.result * hand.bet
+        if self.player.insurance:
+            self.player.cash = self.insurance_payout() * self.player.bet
+            self.player.insurance = False
+        print('round finished')
 
-    def split(self, hand):
-        index = self.player.hands.index(hand)
-        self.player.hands.remove(hand)
-        i = 0
-        for card in hand.cards:
-            split_hand = Hand(card, self.player.bet)
-            split_hand.split = True
-            self.player.hands.insert(index + i, split_hand)
-            i += 1
-        #self.play_all_hands()  
+        
+    def evaluate(self, pl_hand, dl_hand):
+        if pl_hand.get_value() == 'BJ':
+            if dl_hand.get_value() == 'BJ':
+                self.result = 1
+                return '---PUSH---'
+            self.result = 2.5
+            return '---YOU HAVE BLACKJACK---'
+        elif dl_hand.get_value() == 'BJ':
+            self.result = 0
+            return '---DEALER HAS BLACKJACK---'
+        if pl_hand.is_bust:
+            self.player.result = 0
+            return "---YOU'RE BUST---"
+        elif dl_hand.is_bust:
+            self.result = 2
+            return '---DEALER BUST---'
+        elif pl_hand.get_value() > dl_hand.get_value():
+            self.result = 2
+            return '---YOU WIN---'
+        elif pl_hand.get_value() == dl_hand.get_value():
+            self.result = 1
+            return '---PUSH---'
+        self.result = 0
+        return '---YOU LOSE---'
 
+    def insurance_payout(self):
+        self.insurance_asked = False
+        if self.dealer.hand.is_blackjack():
+            return 1
+        return 0 
 
 
 
@@ -293,53 +346,6 @@ class Controller:
             player.insurance = False
         player.hands = []
 """
-
-
-def show_hands(player, dealer):
-    show_dealer_hand(dealer)
-    show_player_hand(player)
-    print()
-
-def show_dealer_hand(dealer):
-    dealer_string = 'Dealer hand:' + str(dealer) + str(dealer.get_value())
-    print('{:<40}'.format(dealer_string), end='')
-
-def show_player_hand(player, indented=False):
-    if indented:
-        print('{:<40}'.format(''), end='')
-    player_string = 'Player hand: ' + str(player) + str(player.get_value())
-    print('{:<40}'.format(player_string), end='')
-
-def evaluate(pl_hand, dl_hand):
-    if pl_hand.get_value() == 'BlackJack':
-        if dl_hand.get_value() == 'BlackJack':
-            player.result = 1
-            return '---PUSH---'
-        player.result = 2.5
-        return '---YOU HAVE BLACKJACK---'
-    elif dl_hand.get_value() == 'BlackJack':
-        player.result = 0
-        return '---DEALER HAS BLACKJACK---'
-    if pl_hand.is_bust:
-        player.result = 0
-        return '---YOU BUST---'
-    elif dl_hand.is_bust:
-        player.result = 2
-        return '---DEALER BUST---'
-    elif pl_hand.get_value() > dl_hand.get_value():
-        player.result = 2
-        return '---YOU WIN---'
-    elif pl_hand.get_value() == dl_hand.get_value():
-        player.result = 1
-        return '---PUSH---'
-    player.result = 0
-    return '---YOU LOSE---'
-
-def insurance_payout():
-    if dealer.hand.is_blackjack():
-        return 1
-    return 0    
-
 
 if __name__ == '__main__':
     pass
