14e7c14a7552ad3c7b021f34e5998bfc1203e042
t1user <tomasz2605@gmail.com>
2024-10-27 15:04:41 +0100

    Callables for game decisions

--- blackjack/engine.py
+++ blackjack/engine.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass, field
 from enum import Enum, Flag, auto
 from functools import cached_property, partial, reduce, wraps
 from operator import ior
-from typing import Any, Callable, ClassVar, Self, TypeVar
+from typing import Any, Callable, ClassVar, Generator, Literal, Self, TypeVar
 
 # ### Rules ###
 MAX_SPLITS = -1  # negative number means no limit
@@ -101,9 +101,7 @@ class Shoe(list[Card]):
     def shuffle(self) -> None:
         self.extend([*DECK * self.decks])
         random.shuffle(self)
-        self._cut_cardcut_card = int(
-            random.randint(*BURN_PERCENT_RANGE) * len(self) / 100
-        )
+        self._cut_card = int(random.randint(*BURN_PERCENT_RANGE) * len(self) / 100)
 
     def deal(self) -> Card:
         return self.pop()
@@ -320,7 +318,7 @@ class Dealer:
 
 @dataclass
 class Player:
-    strategy: GameStrategy
+    strategy: GameStrategy | None
     betting_strategy: BettingStrategy
     cash: float = PLAYER_CASH
     number_of_hands: int = 1
@@ -362,6 +360,11 @@ class PlayDecision(Flag):
         return reduce(
             ior, [flag for flag, predicate in zip(cls, predicates) if predicate]
         )
+
+
+class State(Enum):
+    DONE = auto()
+    NOT_DONE = auto()
 
 
 @dataclass
@@ -464,26 +467,49 @@ class HandPlay:
     def done(self) -> None:
         self._is_done = True
 
-    def surrender(self, dealer: Dealer) -> None:
+    def surrender(self, dealer: Dealer) -> State:
         self.credit_bet(0.5)
         self.hand = Hand()
         self.done()
+        return State.DONE
 
-    def play_insurance(self, dealer: Dealer) -> None:
-        if self.player.strategy.insurance(dealer.hand, self.hand):
-            try:
-                self.charge_bet(0.5)
-                self.insurance = 0.5 * self.betsize
-            except NotEnoughCash:
-                pass
+    def play_insurance(self, dealer: Dealer) -> State | Callable[[bool], State]:
+        if self.player.cash < 0.5 * self.betsize:
+            return State.DONE
 
-    def play(self, dealer: Dealer) -> list[Self] | Self | None:
+        if self.player.strategy is None:
+            return self.process_insurance_decision
+        else:
+            return self.process_insurance_decision(
+                self.player.strategy.insurance(dealer.hand, self.hand)
+            )
+
+    def process_insurance_decision(self, decision: bool) -> State:
+        if decision:
+            self.charge_bet(0.5)
+            self.insurance = 0.5 * self.betsize
+        return State.DONE
+
+    def play(
+        self, dealer: Dealer
+    ) -> (
+        list[Self] | Self | State | Callable[[PlayDecision], list[Self] | Self | State]
+    ):
         if self.allowed_choices is None:
-            return None
-        decision = self.player.strategy.play(
-            dealer.hand, self.hand, self.allowed_choices
-        )  # type: ignore
+            return State.DONE
+        if self.player.strategy is None:
+            return partial(self.process_decision, dealer)
+        else:
+            return self.process_decision(
+                dealer,
+                self.player.strategy.play(dealer.hand, self.hand, self.allowed_choices),
+            )
+
+    def process_decision(
+        self, dealer: Dealer, decision: PlayDecision
+    ) -> list[Self] | Self | State:
         try:
+            assert self.allowed_choices is not None
             assert decision in self.allowed_choices
         except AssertionError as e:
             raise GameError from e
@@ -502,11 +528,12 @@ class HandPlay:
             case _:
                 raise GameError("Unknown play decision")
 
-    def double(self, dealer: Dealer) -> None:
+    def double(self, dealer: Dealer) -> State:
         self.charge_bet(1)
         self.betsize *= 2
         dealer.deal(self)
         self.done()
+        return State.DONE
 
     def split(self, dealer: Dealer) -> list[Self]:
         self.charge_bet()
@@ -531,10 +558,11 @@ class HandPlay:
         dealer.deal(self)
         return self
 
-    def stand(self, dealer: Dealer) -> None:
+    def stand(self, dealer: Dealer) -> State:
         self.done()
+        return State.DONE
 
-    def eval_hand(self, dealer: Dealer) -> None:
+    def eval_hand(self, dealer: Dealer) -> State:
         dealer_hand = dealer.hand
         if self.hand > dealer_hand:
             if self.hand.is_blackjack():
@@ -544,13 +572,12 @@ class HandPlay:
         elif self.hand == dealer_hand:
             self.credit_bet(1)
         self.player.credit(self._winnings)
+        return State.DONE
 
-    def eval_insurance(self, dealer: Dealer) -> None:
-        if not self.insurance:
-            return
-        dealer_hand = dealer.hand
-        if dealer_hand.is_blackjack():
+    def eval_insurance(self, dealer: Dealer) -> State:
+        if self.insurance and dealer.hand.is_blackjack():
             self.credit(self.insurance * 3)
+        return State.DONE
 
     def __iadd__(self, card: Card) -> Self:
         self.hand.append(card)
@@ -616,20 +643,23 @@ class HandPlay:
 @dataclass
 class TablePlay:
     _hands: list[HandPlay] = field(default_factory=list, repr=False)
-    _done: list[HandPlay] = field(default_factory=list, init=False, repr=False)
+    _done: list[HandPlay] = field(default_factory=list, repr=False)
+    _in_progress: HandPlay | None = field(default=None, repr=False)
     _methods: ClassVar = (
         "play_insurance",
         "play",
         "eval_hand",
         "eval_insurance",
     )
+    choices: PlayDecision | None = None
 
     def __iter__(self) -> Self:
         return self
 
     def __next__(self) -> HandPlay:
         try:
-            return self._hands.pop()
+            self._in_progress = self._hands.pop()
+            return self._in_progress
         except IndexError:
             self._hands = self._done
             self._done = []
@@ -641,14 +671,25 @@ class TablePlay:
         else:
             raise AttributeError
 
-    def run_all_hands(self, name: str, dealer: Dealer):
+    def run_all_hands(self, name: str, dealer: Dealer) -> Generator[None | Callable]:
+        # This is a generator that yields a callable if it needs a decision
+        # and raises StopIteration otherwise
+        # Hand that returns State.DONE is done, otherwise it needs further processing
         for hand_play in self:
             method = getattr(hand_play, name)
             hand_hands_or_none = method(dealer)
-            if hasattr(hand_hands_or_none, "__iter__"):
-                self._hands.extend(hand_hands_or_none)
-            elif hand_hands_or_none is None:
+
+            if callable(hand_hands_or_none):
+                self.choices = hand_play.allowed_choices
+                feedback = yield hand_hands_or_none
+                if feedback is not None:
+                    hand_hands_or_none = feedback
+
+            if hand_hands_or_none is State.DONE:
                 self._done.append(hand_play)
+            elif hasattr(hand_hands_or_none, "__iter__"):
+                assert not callable(hand_hands_or_none)
+                self._hands.extend(hand_hands_or_none)
             else:
                 try:
                     assert isinstance(hand_hands_or_none, HandPlay)
@@ -657,10 +698,14 @@ class TablePlay:
                         f"Wrong hand received from play: {hand_hands_or_none}"
                     ) from e
                 self._hands.append(hand_hands_or_none)
+            self._in_progress = None
 
     @property
     def hands(self):
-        return [*self._hands, *self._done]
+        if self._in_progress is None:
+            return [*self._hands, *self._done]
+        else:
+            return [*self._hands, self._in_progress, *self._done]
 
     def deal_card(self, dealer: Dealer):
         for hand in self.hands:
@@ -671,30 +716,99 @@ class TablePlay:
 
 
 @dataclass
+class DecisionCallable:
+    callable: Callable
+
+    def __call__(self, decision):
+        return self.callable(decision)
+
+
+class InsuranceDecisionCallable(DecisionCallable):
+    pass
+
+
+class PlayDecisionCallable(DecisionCallable):
+    pass
+
+
+@dataclass
 class Round:
     dealer: Dealer
     table: TablePlay
+    decision_callable: DecisionCallable | None = None
 
-    def shuffle(self) -> Self:
+    @property
+    def choices(self) -> PlayDecision:
+        try:
+            assert self.table.choices
+        except AssertionError as e:
+            raise GameError("Accessing choices when none are available") from e
+        return self.table.choices
+
+    def process_decision(
+        self,
+        gen: Generator,
+        next_function: Callable,
+        decision_type: Literal["play", "insurance"] = "play",
+    ) -> Self | None:
+
+        try:
+            decision_function = next(gen)
+        except StopIteration:
+            return next_function()
+        else:
+            self.make_callable(gen, next_function, decision_function, decision_type)
+
+    def make_callable(
+        self,
+        gen: Generator,
+        next_function: Callable,
+        decision_function: Callable,
+        decision_type: Literal["play", "insurance"],
+    ):
+
+        def inner(decision: PlayDecision | bool):
+            self.decision_callable = None
+            try:
+                next_decision_function = gen.send(decision_function(decision))
+            except StopIteration:
+                next_function()
+            else:
+                self.make_callable(
+                    gen, next_function, next_decision_function, decision_type
+                )
+
+        decision_class = {
+            "play": PlayDecisionCallable,
+            "insurance": InsuranceDecisionCallable,
+        }.get(decision_type)
+
+        if decision_class:
+            self.decision_callable = decision_class(inner)
+
+    def shuffle(self) -> Self | None:
         self.dealer.shuffle()
-        return self
+        return self.deal()
 
-    def deal(self) -> Self:
+    def deal(self) -> Self | None:
         self.table.deal_card(self.dealer)
         self.dealer.deal_self()
         self.table.deal_card(self.dealer)
-        return self
+        return self.offer_insurance()
 
-    def offer_insurance(self) -> Self:
+    def offer_insurance(self) -> Self | None:
         if self.dealer.has_ace:
-            self.table.play_insurance(self.dealer)
-        return self
+            insurance_result = self.table.play_insurance(self.dealer)
+            return self.process_decision(
+                insurance_result, self.player_play, "insurance"
+            )
+        return self.player_play()
 
-    def player_play(self) -> Self:
-        self.table.play(self.dealer)
-        return self
+    def player_play(self) -> Self | None:
+        play_results = self.table.play(self.dealer)
+        return self.process_decision(play_results, self.dealer_play)
 
-    def dealer_play(self) -> Self:
+    def dealer_play(self) -> Self | None:
         # dealer takes card if there is at least one non-busted hand
         # surrendered hand (value == 0) shouldn't be a reason to take card
         # or insurance in play:
@@ -706,26 +820,30 @@ class Round:
                 is PlayDecision.HIT
             ):
                 self.dealer.deal_self()
-        return self
+        return self.eval_insurance()
 
-    def eval_insurance(self) -> Self:
-        self.table.eval_insurance(self.dealer)
-        return self
+    def eval_insurance(self) -> Self | None:
+        evi = self.table.eval_insurance(self.dealer)
+        return self.process_decision(evi, self.eval_hands)
 
-    def eval_hands(self) -> Self:
-        self.table.eval_hand(self.dealer)
-        return self
+    def eval_hands(self) -> Self | None:
+        ev = self.table.eval_hand(self.dealer)
+        return self.process_decision(ev, lambda: self)
 
-    def play(self) -> Self:
-        return (
-            self.shuffle()
-            .deal()
-            .offer_insurance()
-            .player_play()
-            .dealer_play()
-            .eval_insurance()
-            .eval_hands()
-        )
+    @property
+    def pipe(self) -> list[Callable]:
+        return [
+            self.shuffle,
+            self.deal,
+            self.offer_insurance,
+            self.player_play,
+            self.dealer_play,
+            self.eval_insurance,
+            self.eval_hands,
+        ]
+
+    def play(self) -> Self | None:
+        return self.shuffle()
 
 
 class NotEnoughCash(Exception):
@@ -752,8 +870,12 @@ class Game:
 
     players: list[Player]
     dealer: Dealer = field(default_factory=Dealer)
+    round: Round = field(init=False)
 
-    def round(self):
+    def __post_init__(self):
+        self.round = Round(self.dealer, TablePlay())
+
+    def make_round(self):
         hand_plays: list[HandPlay] = []
         for player in self.players:
             for _ in range(player.number_of_hands):
@@ -762,8 +884,9 @@ class Game:
                     hand_plays.append(hand_play)
         return Round(self.dealer, TablePlay(hand_plays))
 
-    def play(self) -> Round:
-        return self.round().play()
+    def play(self) -> Round | None:
+        self.round = self.make_round()
+        return self.round.play()
 
     def loop_play(self):
         while True:
